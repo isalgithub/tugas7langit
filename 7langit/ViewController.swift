0c03a14ec32c9312fc7616e81b4e6fddf19d2bdc
Faishal Alif Fadhillah <emailkerjaisal@gmail.com>
2018-09-08 11:39:20 +0700

    Initial Commit

---
+++ 7langit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7langit
+//
+//  Created by Faishal Alif on 9/8/18.
+//  Copyright © 2018 Faishal Alif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
